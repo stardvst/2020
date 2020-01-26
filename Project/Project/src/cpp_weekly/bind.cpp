2cf9160183162845a3df61cd3691dc11a7b50577
Lilia <lilia.manukyan@hotmail.com>
2020-01-26 08:51:52 +0400

    replace std::bind with lambda, thanks

--- Project/Project/src/cpp_weekly/bind.cpp
+++ Project/Project/src/cpp_weekly/bind.cpp
@@ -44,6 +44,15 @@ int main()
 	const auto fTemplate = std::bind(&printTemplate<int>, std::placeholders::_2, std::placeholders::_1);
 	fTemplate("hello world", 7);
 
+	// better, replace with lambda: 20% less ram, 20% less compile time
+	const auto lTemplate =
+		[](auto &&arg1, auto &&arg2, auto &&.../* swallow params */)
+	{
+		printTemplate(std::forward<decltype(arg2)>(arg2), std::forward<decltype(arg1)>(arg1));
+	};
+	lTemplate("hello world from lambda", 7, 1234567);
+	lTemplate("hello float", 7.5); // don't need to specify exact type
+
 	// create a wrapper
 	std::function<void(const std::string &, int)> wrapper(fTemplate);
 	wrapper("hello world", 8);
