9e1d03e156ce18abb9cc26af6c4fa2eadc33401e
Lilia <lilia.manukyan@hotmail.com>
2020-02-09 09:38:21 +0400

    fast Fibonacci sequence with stateful lambdas

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,18 +1,44 @@
-struct Lambda
-{
-	auto operator()() const
-	{
-		return 5;
-	}
-};
+#include <iostream>
 
 int main()
 {
-	// without -Ox, clang generates more code than gcc
-	return[]
+	int i = 1;
+	auto l = [i]
 	{
-		return 5;
-	}();
+		return i;
+	};
 
-	// return Lambda()();
+	auto l2 = [&i]
+	{
+		return ++i;
+	};
+
+	auto l3 = [i]() mutable
+	{
+		return ++i;
+	};
+
+	// c++14, stateful lambda
+	auto l4 = [counter = 0]() mutable
+	{
+		return ++counter;
+	};
+	l4();
+	l4();
+
+	// Fibonacci sequence, probably fastest way
+	auto l5 = [a1 = 0, a2 = 1]() mutable
+	{
+		a1 = std::exchange(a2, a1 + a2);
+		return a1;
+	};
+
+	std::cout << l() << '\n';
+	std::cout << l2() << '\n';
+	std::cout << l3() << '\n';
+	std::cout << l4() << '\n';
+
+	std::cout << "Fibonacci sequence: ";
+	for (auto i = 0; i < 6; ++i)
+		std::cout << l5() << ' ';
 }
