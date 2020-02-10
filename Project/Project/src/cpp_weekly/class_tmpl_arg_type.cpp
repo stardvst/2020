a4ff78c645cbad9f002a379990eacade4776af7f
Lilia <lilia.manukyan@hotmail.com>
2020-02-10 20:21:21 +0400

    creating custom deduction guides

--- Project/Project/src/cpp_weekly/class_tmpl_arg_type.cpp
+++ Project/Project/src/cpp_weekly/class_tmpl_arg_type.cpp
@@ -1,22 +1,42 @@
 #include <tuple>
 #include <functional>
 
+namespace std
+{
+	template <typename Ret, typename Class, typename ...Args>
+	function(Ret(Class::*)(Args...))->function<Ret(Class &, Args...)>;
+
+	template <typename Ret, typename Class, typename ...Args>
+	function(Ret(Class:: *)(Args...) const)->function<Ret(const Class &, Args...)>;
+}
+
 void test()
 {
 }
 
+void test2(int, char)
+{
+}
+
+struct myClass
+{
+	void member()
+	{
+	}
+
+	void constMember() const
+	{
+	}
+};
+
 int main()
 {
-	// pre-c++17
-	std::tuple<int, int> t1(1, 2);
-	const auto t2 = std::make_tuple(3, 4);
-
-	// c++17
-	std::tuple t3(5, 6);
-	std::tuple t4([](){}, [](){});
-
-	// compile error: mismatched set of types
-	//return t3 == t4;
-
+	// no deduction guide needed as standard provides one
 	std::function f(&test);
+	std::function f2(&test2);
+	f2(1, 'a');
+
+	// need custom deduction guides
+	std::function m(&myClass::member);
+	std::function cm(&myClass::constMember);
 }
