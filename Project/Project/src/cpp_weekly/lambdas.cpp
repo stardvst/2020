3829e576b6e4218974391cf1195ad589411a9981
Lilia <lilia.manukyan@hotmail.com>
2020-02-22 09:29:06 +0400

    storage duration with lambdas

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,40 +1,48 @@
 #include <iostream>
-#include <utility>
+#include <thread>
+#include <future>
 
 int main()
 {
-	auto fib = [a = 0, b = 1]() mutable
+	auto l1 = [val = 0]() mutable
 	{
-		struct Results
-		{
-			int &a;
-			int &b;
-
-			Results next(int times = 1)
-			{
-				while (times--)
-				{
-					a = std::exchange(b, a + b);
-					return *this;
-				}
-			}
-
-			operator int()
-			{
-				return a;
-			}
-		};
-
-		return Results{ a, b }.next();
+		return ++val;
 	};
 
-	std::cout << fib()/*.a*/ << '\n';
-	std::cout << fib()/*.a*/ << '\n';
-	std::cout << fib()/*.a*/ << '\n';
-	std::cout << fib()/*.a*/ << '\n';
+	// cannot copy with initial state
+	// is not thread-safe
+	auto l2 = []
+	{
+		static auto val = 0;
+		return ++val;
+	};
 
-	// ++
-	std::cout << fib().next()/*.a*/ << '\n';
+	// will increment the same val as in l2
+	auto l3 = l2;
 
-	std::cout << fib().next(3)/*.a*/ << '\n';
+	std::cout << l1() << '\n';
+	std::cout << l1() << '\n';
+
+	std::cout << l2() << '\n';
+	std::cout << l2() << '\n';
+
+	std::cout << l3() << '\n';
+	std::cout << l3() << '\n';
+
+	// val is shared in across main and other threads
+	auto f1 = std::async(std::launch::async, l2);
+	f1.get();
+	std::cout << l2() << '\n';
+
+	auto l4 = []
+	{
+		thread_local auto val = 0;
+		return ++val;
+	};
+
+	auto f2 = std::async(std::launch::async, l4);
+	f2.get();
+
+	std::cout << l4() << '\n';
+	std::cout << l4() << '\n';
 }
