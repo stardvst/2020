be977b841c9bc344c9acfd4b95690251b01924b5
Lilia <lilia.manukyan@hotmail.com>
2020-03-01 11:18:53 +0400

    regex with std::regex::optimize

--- Project/Project/src/cpp_weekly/regex.cpp
+++ Project/Project/src/cpp_weekly/regex.cpp
@@ -12,9 +12,12 @@ void dumpRegex(const std::cmatch &match)
 
 int main()
 {
-	std::regex rx(R"((\d+): \[([^\]]+)\] (.*))");
-
+	// this is slower than without optimize flag;
+	// regex and matches should be constructed only as needed,
+	// i.e. should be outside for loop, const and maybe static
+	std::regex rx(R"((\d+): \[([^\]]+)\] (.*))", std::regex::optimize);
 	std::cmatch match;
+
 	std::regex_match("123: [error] Hello World", match, rx);
 
 	dumpRegex(match);
