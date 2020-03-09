28f5d134037101431182dff636d6a279400b74f9
Lilia <lilia.manukyan@hotmail.com>
2020-03-09 09:01:40 +0400

    lambdas as comparators for containers

--- Project/Project/src/cpp_weekly/containers.cpp
+++ Project/Project/src/cpp_weekly/containers.cpp
@@ -17,6 +17,31 @@ struct Compare
 
 int main()
 {
-	std::set<MyData, Compare> set;
-	set.insert(MyData{ "Bob" });
+	// error: lambda expression in template argument
+	// argument expects type, but we passed value, so added decltype
+	// but error is now about type instead of value
+	//std::set < MyData, decltype([](const MyData &lhs, const MyData &rhs)
+	//{
+	//	return lhs.key < rhs.key;
+	//}) > set;
+
+	const auto lambda = [](const MyData & lhs, const MyData & rhs)
+	{
+		return lhs.key < rhs.key;
+	};
+
+	// cannot take type of lambda
+	//const decltype(lambda) l2;
+	//std::set<MyData, decltype(lambda)> set;
+
+	// pass lambda to set constructor
+	std::set<MyData, decltype(lambda)> set1(lambda);
+	set1.insert(MyData{ "Bob" });
+
+	// c++17
+	std::set set2({ MyData{ "Bob" } }, // initializer list
+		[](const MyData &lhs, const MyData &rhs) // comparator
+	{
+		return lhs.key < rhs.key;
+	});
 }
