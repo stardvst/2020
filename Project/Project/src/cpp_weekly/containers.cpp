d0e0671ab356b6b10b16b4bdab38799e10101faa
Lilia <lilia.manukyan@hotmail.com>
2020-03-13 21:53:51 +0400

    transparent lambda comparators

--- Project/Project/src/cpp_weekly/containers.cpp
+++ Project/Project/src/cpp_weekly/containers.cpp
@@ -1,72 +1,67 @@
 #include <string>
 #include <set>
 
-struct MyData
+struct Person
 {
-	std::string key;
-	std::string value;
+	std::string name;
 };
 
-struct Compare
+template <typename Type, typename Comparator>
+auto makeSet(Comparator &&comparator)
 {
-	bool operator()(const MyData &lhs, const MyData &rhs) const
-	{
-		return lhs.key < rhs.key;
-	}
+	return std::set<Type, std::decay_t<Comparator>>{
+		std::forward<Comparator>(comparator)};
+}
 
-	// transparent comparator, i.e. doesn't have to compare with MyData,
-	// can be any other type
-	template <typename T>
-	bool operator()(const MyData &lhs, const T &str) const
+template <typename Type, typename ...Comparator>
+auto makeSetVar(Comparator &&...comparator)
+{
+	// need to expand parameter pack, inherit from all comparators
+	struct Compare : std::decay_t<Comparator>...
 	{
-		return lhs.key < str;
-	}
+		// pull call operators from all comparators
+		using std::decay_t<Comparator>::operator()...;
+		using is_transparent = int;
 
-	template <typename T>
-	bool operator()(const T &str, const MyData &rhs) const
-	{
-		return str < rhs.key;
-	}
-
-	// activate transparent comparators
-	using is_transparent = int;
-};
+		// cannot have member template in local class,
+		// so we'll pass compare object to set ctor
+		//template <typename ...T>
+		//Compare(T &&...t)
+		//{
+		//}
+	};
+	return std::set<Type, Compare>{Compare{ std::forward<Comparator>(comparator)... }};
+}
 
 int main()
 {
-	// error: lambda expression in template argument
-	// argument expects type, but we passed value, so added decltype
-	// but error is now about type instead of value
-	//std::set < MyData, decltype([](const MyData &lhs, const MyData &rhs)
-	//{
-	//	return lhs.key < rhs.key;
-	//}) > set;
-
-	const auto lambda = [](const MyData & lhs, const MyData & rhs)
+	auto comparator1 = [](const Person &lhs, const Person &rhs)
 	{
-		return lhs.key < rhs.key;
+		return lhs.name < rhs.name;
 	};
 
-	// cannot take type of lambda
-	//const decltype(lambda) l2;
-	//std::set<MyData, decltype(lambda)> set;
+	std::set<Person, decltype(comparator1)> set{ std::move(comparator1) };
 
-	// pass lambda to set constructor
-	std::set<MyData, decltype(lambda)> set1(lambda);
-	set1.insert(MyData{ "Bob" });
-
-	// c++17
-	std::set set2({ MyData{ "Bob" } }, // initializer list
-		[](const MyData &lhs, const MyData &rhs) // comparator
+	// make set
+	auto set1 = makeSet<Person>([](const Person &lhs, const Person &rhs)
 	{
-		return lhs.key < rhs.key;
+		return lhs.name < rhs.name;
 	});
 
-	std::set<MyData, Compare> set3;
-	set3.insert(MyData{ "Bob" });
+	// make set variadic
+	auto set2 = makeSetVar<Person>(
+		[](const Person &lhs, const Person &rhs)
+	{
+		return lhs.name < rhs.name;
+	},
+		[](const Person &lhs, const auto &rhs)
+	{
+		return lhs.name < rhs;
+	},
+		[](const auto &lhs, const Person &rhs)
+	{
+		return lhs < rhs.name;
+	});
 
-	const auto count1 = set3.count(MyData{ "Bob" }); // have to create an object
-
-	// works only with overloads
- 	auto count2 = set3.count(std::string("Bob"));
+	const auto count = set2.count("Bob");
 }
