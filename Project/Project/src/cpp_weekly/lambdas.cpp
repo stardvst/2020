a174a42e6aa69cd9359d5434502fbfcd06a8a97a
Lilia <lilia.manukyan@hotmail.com>
2020-03-14 09:53:13 +0400

    lambda to function pointer conversion in function object storage

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,19 +1,30 @@
 #include <string>
-#include <set>
-
-struct Person
-{
-	std::string name;
-};
+#include <functional>
 
 int main()
 {
 	// lambdas can be converted into function pointers
 	// it must be non-capturing lambda (which has state)
-	using fp = bool(*)(const Person & lhs, const Person & rhs);
-
-	std::set<Person, fp> set{ [](const Person &lhs, const Person &rhs)
+	std::vector<std::function<int(int, int)>> functions1;
+	functions1.emplace_back([](int i, int j)
 	{
-		return lhs.name < rhs.name;
-	} };
+		return i + j;
+	});
+	functions1.emplace_back([](int i, int j)
+	{
+		return i - j;
+	});
+
+	// above has overhead for generating type erasure, instead take
+	// advantage of lambda to function pointer conversion.
+	// in this case less code is generated
+	std::vector<int (*)(int, int)> functions2;
+	functions2.emplace_back([](int i, int j)
+	{
+		return i + j;
+	});
+	functions2.emplace_back([](int i, int j)
+	{
+		return i - j;
+	});
 }
