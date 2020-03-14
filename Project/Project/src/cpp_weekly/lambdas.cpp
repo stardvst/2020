6027113a48817ea0b27d2b705b2682ae7cc5d8c0
Lilia <lilia.manukyan@hotmail.com>
2020-03-14 09:28:46 +0400

    lambda to function pointer conversion as set comparator

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,48 +1,19 @@
-#include <iostream>
-#include <thread>
-#include <future>
+#include <string>
+#include <set>
+
+struct Person
+{
+	std::string name;
+};
 
 int main()
 {
-	auto l1 = [val = 0]() mutable
+	// lambdas can be converted into function pointers
+	// it must be non-capturing lambda (which has state)
+	using fp = bool(*)(const Person & lhs, const Person & rhs);
+
+	std::set<Person, fp> set{ [](const Person &lhs, const Person &rhs)
 	{
-		return ++val;
-	};
-
-	// cannot copy with initial state
-	// is not thread-safe
-	auto l2 = []
-	{
-		static auto val = 0;
-		return ++val;
-	};
-
-	// will increment the same val as in l2
-	auto l3 = l2;
-
-	std::cout << l1() << '\n';
-	std::cout << l1() << '\n';
-
-	std::cout << l2() << '\n';
-	std::cout << l2() << '\n';
-
-	std::cout << l3() << '\n';
-	std::cout << l3() << '\n';
-
-	// val is shared in across main and other threads
-	auto f1 = std::async(std::launch::async, l2);
-	f1.get();
-	std::cout << l2() << '\n';
-
-	auto l4 = []
-	{
-		thread_local auto val = 0;
-		return ++val;
-	};
-
-	auto f2 = std::async(std::launch::async, l4);
-	f2.get();
-
-	std::cout << l4() << '\n';
-	std::cout << l4() << '\n';
+		return lhs.name < rhs.name;
+	} };
 }
