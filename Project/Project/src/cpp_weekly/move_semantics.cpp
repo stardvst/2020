840573c117a1cbe7a2b00511220a415f2f23b353
Lilia <lilia.manukyan@hotmail.com>
2020-03-21 15:41:41 +0400

    disabling move from const

--- Project/Project/src/cpp_weekly/move_semantics.cpp
+++ Project/Project/src/cpp_weekly/move_semantics.cpp
@@ -1,43 +1,20 @@
 #include <iostream>
-#include <string>
 
 struct S
 {
-	S(const S &)
-	{
-		std::cout << "copy ctor\n";
-	}
-
-	S(S &&)
-	{
-		std::cout << "move ctor\n";
-	}
-
-	S &operator=(const S &) = default;
-	S &operator=(S &&) = default;
-
 	S() = default;
-	~S() = default;
 
-	std::string str = "Hello world long string";
+	// delete this constructor!
+	S(const S &&) = delete;
+	S &operator=(const S &&) = delete;
 };
 
 int main()
 {
-	std::string s1 = "Hello World!";
-	const std::string s2 = "Hello World!";
+	S s1;
 
-	std::string &&ref1 = std::move(s1); // move returns &&
-	const std::string &&ref2 = std::move(s2); // move returns const &&...
-	//^^^
-	// so const is mandatory
+	S s2 = std::move(s1); // copy instead of move, so disable
 
-	S o1;
-	S o2(std::move(o1)); // move ctor expects S &&, ok
-
-	// const S && is passed to move ctor, which cannot be bind to
-	// non-const r-value reference (which move ctor expects), and
-	// so it uses normal overloading rules, and uses copy ctor instead!
-	const S o3;
-	S o4(std::move(o3));
+	S s3;
+	s3 = std::move(s1);
 }
