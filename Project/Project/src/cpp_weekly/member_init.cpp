922b8bc32eb6383cf89dae52473f235b90389887
Lilia <lilia.manukyan@hotmail.com>
2020-03-22 08:28:18 +0400

    the power of =delete

--- Project/Project/src/cpp_weekly/member_init.cpp
+++ Project/Project/src/cpp_weekly/member_init.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 
-struct S
+struct S1
 {
-	// c++20
-	int i : 3 = 0;
-	int j : 5 = 0;
-
-	// before
-	S() : i(0), j(0)
+	constexpr void f(int) const noexcept
 	{
 	}
+
+	constexpr void f(double) const noexcept = delete;
+};
+
+struct S2
+{
+	template <typename T>
+	constexpr void f(T) const noexcept
+	{
+	}
+
+	// specifically delete the double version
+	constexpr void f(double) const noexcept = delete;
 };
 
 int main()
 {
-	// 4 bytes, we're getting padding
-	std::cout << sizeof(S);
+	S1 s1;
+	s1.f(1);
+	//s1.f(1.0); // deleted
+	//s1.f(1.0f); // implicit conversion to double
+	//s1.f(2ll); // ambiguous
+
+	S2 s2;
+	s2.f(1);
+	//s2.f(1.0);
+	s2.f(1.0f);
+	s2.f(2ll);
 }
