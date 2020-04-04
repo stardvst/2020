56b3a04ba7373136abaeb60ac8317b24a34976f8
Lilia <lilia.manukyan@hotmail.com>
2020-04-04 10:25:27 +0400

    lambdas with dtors

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -3,21 +3,48 @@
 
 int main()
 {
-	auto l = [val = 2]{ return val; };
-
-	// cannot access private member declared in class
-	// 'main::<lambda_7353eea49db4595be34d9f57d4c82dc6>'
-	// l.val = 4;
-
+	auto l = [i = 10]{ return i; };
 	std::cout << l() << '\n';
 
-	std::array arr = { 1, 2, 3, 4, 5 };
-	int sum = 0;
-	for (const auto &v : arr)
+	// lambda capture can be any expression
+	// lambda is expression => capture can be lambda
+	auto l2 = [i = [] { return 10; }]
 	{
-		sum += v;
-		//++__for_begin;
-	}
+		return i;
+	};
 
-	std::cout << sum << '\n';
+	// invoke lambda returned from lambda
+	std::cout << l2()() << '\n';
+
+	// capture type
+	auto l3 = [i = []
+	{
+		struct S
+		{
+			int val = 10;
+			S()
+			{
+				puts("S()");
+			}
+			S(const S &)
+			{
+				puts("S(const S &)");
+			}
+			~S()
+			{
+				puts("~S()");
+			}
+		};
+		return S{};
+	}()]
+	{
+		return i;
+	};
+
+	// lambda returns object
+	//std::cout << l3().val << '\n';
+
+	// lambdas are objects with lifetimes
+	// copying lambda => copies all of its captures
+	//auto l4 = l3;
 }
