0129e5c31090d157e7091e04b49d47597a66315a
Lilia <lilia.manukyan@hotmail.com>
2020-04-10 22:08:57 +0400

    lambdas in fold expressions

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
-#include <vector>
+
+template <typename ...T>
+auto doStuff1(T ...t)
+{
+	return (t + ...);
+}
+
+template <typename ...T>
+auto doStuff2(T ...t)
+{
+	return ([t]() { return t; }() +...);
+}
 
 int main()
 {
-	auto l1 = [](auto first, auto second)
-	{
-		return first + second;
-	};
-
-	std::cout << l1(2, 4) << '\n';
-	std::cout << l1(2.1, 4.9) << '\n';
-
-	// same as above because auto uses the same rules
-	// as template type deduction
-	auto l2 = []<typename T>(T first, T second)
-	{
-		return first + second;
-	};
-
-	std::cout << l2(2, 4) << '\n';
-	//std::cout << l2(2, 4.5) << '\n'; // doesn't work
-	//std::cout << l2(2.1, 4) << '\n'; // doesn't work
-
-	auto l3 = []<typename T>(const std::vector<T> &v)
-	{
-		return v.size();
-	};
-
-	std::vector<int> vec{ 1, 2, 3, 4 };
-	std::cout << l3(vec) << '\n';
+	std::cout << doStuff1(1, 2, 3, 4, 5) << '\n';
+	std::cout << doStuff2(1, 2, 3, 4, 5) << '\n';
 }
