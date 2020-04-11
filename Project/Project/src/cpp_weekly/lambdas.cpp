8b40300c63773f926ea12cf0271b2a135c2dccf8
Lilia <lilia.manukyan@hotmail.com>
2020-04-11 09:18:59 +0400

    different lambdas implementation

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,19 +1,67 @@
 #include <iostream>
+#include <typeinfo>
 
-template <typename ...T>
-auto doStuff1(T ...t)
+struct __main_lambda_0
 {
-	return (t + ...);
-}
+	constexpr auto operator()() const
+	{
+		return 5;
+	}
+};
 
-template <typename ...T>
-auto doStuff2(T ...t)
+struct __main_lambda_1
 {
-	return ([t]() { return t; }() +...);
-}
+	constexpr auto operator()(int i) const
+	{
+		return 5 + i;
+	}
+};
+
+struct __main_lambda_2
+{
+	int val;
+	constexpr auto operator()(int i) const
+	{
+		return 5 + i + val;
+	}
+};
+
+struct __main_lambda_3
+{
+	int val;
+	constexpr auto operator()(int i)
+	{
+		return 5 + i + ++val;
+	}
+};
+
+struct __main_lambda_4
+{
+	int val;
+
+	template <typename T>
+	constexpr auto operator()(T i)
+	{
+		return 5 + i + ++val;
+	}
+};
 
 int main()
 {
-	std::cout << doStuff1(1, 2, 3, 4, 5) << '\n';
-	std::cout << doStuff2(1, 2, 3, 4, 5) << '\n';
+	auto l0 = []() { return 5; };
+	puts(typeid(l0).name());
+
+	auto l1 = [](int i) { return 5 + i; };
+	puts(typeid(l1).name());
+
+	// can't mutate val
+	int val = 10;
+	auto l2 = [val](int i) { return 5 + i + val; };
+	puts(typeid(l2).name());
+
+	auto l3 = [val](int i) mutable { return 5 + i + ++val; };
+	puts(typeid(l3).name());
+
+	auto l4 = [val](auto i) mutable { return 5 + i + ++val; };
+	puts(typeid(l4).name());
 }
