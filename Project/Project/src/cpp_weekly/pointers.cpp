f6a56100e3e0d3c2aed49fb7f082c826027c93df
Lilia <lilia.manukyan@hotmail.com>
2020-04-18 08:38:49 +0400

    c++20's to_address

--- Project/Project/src/cpp_weekly/pointers.cpp
+++ Project/Project/src/cpp_weekly/pointers.cpp
@@ -1,58 +1,21 @@
 #include <iostream>
+#include <memory>
+#include <vector>
 
 int main()
 {
-	int i = 5;
-	int j = 10;
+	auto printAddress = [](const auto &p)
+	{
+		std::cout << std::to_address(p) << '\n';
+	};
 
-	// refs
-	int &iRef = i;
-	std::cout << i << ' ' << iRef << '\n';
+	auto intPtr = std::make_unique<int>(15);
+	printAddress(intPtr); // heap address
 
-	iRef = 11;
-	std::cout << i << ' ' << iRef << '\n';
+	int i{ 17 };
+	//printAddress(i); // can't do, to_address expects pointer-like thing
+	printAddress(&i); // stack address
 
-	// pointers
-	int *iPtr = &i;
-	std::cout << i << ' ' << *iPtr << '\n';
-
-	iPtr = &j;
-	std::cout << j << ' ' << *iPtr << '\n';
-
-	// refs to pointers
-	//int &*refToPtr = iPtr; // pointer to ref is not allowed
-	int *&refToPtr = iPtr; // okay
-	std::cout << "ref to ptr: " << *refToPtr << ", j: " << j << '\n';
-
-	// assign address of i to this which is a ref that points to
-	// iPtr which contains the address of j, so now iPtr contains
-	// the address of i
-	refToPtr = &i;
-	std::cout << "ref to ptr: " << *refToPtr << ", i: " << i << '\n';
-
-	// the const-ness
-	//int *&ptrRef1 const = iPtr; // declaration requires an initializer
-
-	int *&const ptrRef2 = iPtr; // okay
-
-	int *const &ptrRef3 = iPtr; // const ptr to ref? or ptr to const value
-	//ptrRef3 = &j; // it's const, cannot re-assign
-	*ptrRef3 = 123; // okay
-	// => it's the pointer that's const, not the value
-
-	// a reference of type "const int *&" (not const-qualified)
-	// cannot be initialized with a value of type "int *"
-	//int const *&ptrRef4 = iPtr;
-
-	const int *constPtr = &i;
-	int const *&ptrRef5 = constPtr;
-
-	// what part of it is const?
-	//*ptrRef5 = 123; // the thing pointer to is const
-	ptrRef5 = &j; // okay
-
-	// very-very const
-	// from cdecl.org:
-	// declare ptrRef6 as reference to const pointer to const int
-	const const int const * const &ptrRef6 = constPtr;
+	std::vector<int> vec{ 1, 2, 3 };
+	printAddress(vec.begin()); // heap address
 }
