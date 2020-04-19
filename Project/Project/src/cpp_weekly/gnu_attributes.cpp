3c16ecec8484b40d86e9cb786f2d2bf897eb4a1b
Lilia <lilia.manukyan@hotmail.com>
2020-04-19 09:33:07 +0400

    misuse of pure function attribute

--- Project/Project/src/cpp_weekly/gnu_attributes.cpp
+++ Project/Project/src/cpp_weekly/gnu_attributes.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 
-[[gnu::pure]] int square(int i);
+struct S
+{
+	[[gnu::pure]] int incr()
+	{
+		return ++val;
+	}
+
+	int non_const()
+	{
+	}
+
+	int val = 0;
+};
 
 int main(int argc, const char *[])
 {
-	// since squre() is pure, it will be called once, then multiplied by 2!
-	std::cout << square(argc) + square(argc) << '\n';
+	S s;
+
+	// prints 0, since compiler optimized away based on gnu::pure
+	// no calls to this are generated
+	s.incr();
+	s.incr();
+	std::cout << s.val << '\n';
+
+	// function call is generated, since it's non-const and
+	// can change object state
+	int i = s.non_const();
+	i += s.non_const();
 }
 
