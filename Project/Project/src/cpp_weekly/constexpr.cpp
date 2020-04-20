cb92c78444d85ad79c5a551c03d17b34f38640e8
Lilia <lilia.manukyan@hotmail.com>
2020-04-20 22:22:00 +0400

    `constexpr` `virtual` members in c++20

--- Project/Project/src/cpp_weekly/constexpr.cpp
+++ Project/Project/src/cpp_weekly/constexpr.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 
-[[nodiscard]] constexpr int add(const int lhs, const int rhs) noexcept
-{
-	return lhs + rhs;
-}
+#include <array>
 
-[[nodiscard]] constexpr int mult(const int lhs, const int rhs) noexcept
+struct Base
 {
-	return lhs * rhs;
-}
+	// why on earth constexpr + virtual?
+	[[nodiscard]] constexpr virtual int get() const = 0;
+};
 
-[[nodiscard]] constexpr auto calc(const int val1, const int val2, const int val3) noexcept
+struct Derived1 : Base
 {
-	return add(val1, mult(val2, val3));
+	[[nodiscard]] constexpr int get() const override
+	{
+		return 5;
+	}
+};
+
+struct Derived2 : Base
+{
+	[[nodiscard]] constexpr int get() const override
+	{
+		return 10;
+	}
+};
+
+constexpr auto getSomeValues()
+{
+	const Derived1 d1_1;
+	const Derived1 d1_2;
+	const Derived1 d1_3;
+	const Derived2 d2_1;
+	const Derived2 d2_2;
+
+	const std::array<const Base *, 5> data
+	{
+		&d1_1, &d1_2, &d1_3, &d2_1, &d2_2
+	};
+
+	int sum = 0;
+	for (const auto *elem : data)
+		sum += elem->get();
+	return sum;
 }
 
 int main()
 {
+	constexpr auto v = getSomeValues();
+	return v;
 }
