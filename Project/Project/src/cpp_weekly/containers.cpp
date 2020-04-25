46922efe676e756183bd75f523f44090d9093e92
Lilia <lilia.manukyan@hotmail.com>
2020-04-25 10:01:36 +0400

    c++20's uniform container erasure

--- Project/Project/src/cpp_weekly/containers.cpp
+++ Project/Project/src/cpp_weekly/containers.cpp
@@ -1,48 +1,13 @@
 #include <iostream>
 #include <vector>
 
-struct S
+void remove5s(std::vector<int> &vec, std::list<int> &lst)
 {
-	S() { puts("S()"); }
-	S(int) { puts("S(int)"); }
-	~S() { puts("~S()"); }
-
-	S(const S &) { puts("S(const S &)"); }
-	S(S &&) noexcept { puts("S(S &&)"); }
-
-	S &operator=(const S &)
-	{
-		puts("S &operator=(const S &)"); return *this;
-	}
-
-	S &operator=(S &&) noexcept
-	{
-		puts("S &operator=(S &&)"); return *this;
-	}
-};
+	//vec.erase(std::remove(begin(vec), end(vec), 5), end(vec));
+	std::erase(vec, 5);
+	std::erase(lst, 5);
+}
 
 int main()
 {
-	std::vector<S> vec;
-	std::cout << "initial capacity: " << vec.capacity() << '\n';
-
-	// S(int), S(&&), ~S(), ~S()
-	// while moves are cheap, they're not as cheap as creating object in-place!
-	//vec.push_back(S(1));
-
-	// S(int), ~S()
-	vec.emplace_back(3);
-	std::cout << "capacity after emplace_back: " << vec.capacity() << '\n';
-
-	// without noexcept, these will result in COPIES,
-	// but with noexcept we're telling that objects can be moved
-	// while maintaining strong exception guarantee!
-	vec.emplace_back(3);
-	std::cout << "capacity after emplace_back: " << vec.capacity() << '\n';
-	vec.emplace_back(3);
-	std::cout << "capacity after emplace_back: " << vec.capacity() << '\n';
-	vec.emplace_back(3);
-	std::cout << "capacity after emplace_back: " << vec.capacity() << '\n';
-	vec.emplace_back(3);
-	std::cout << "capacity after emplace_back: " << vec.capacity() << '\n';
 }
