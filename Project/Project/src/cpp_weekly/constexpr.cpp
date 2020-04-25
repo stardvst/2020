29631c226bef3c15f09df0ce1847a39cdec7180b
Lilia <lilia.manukyan@hotmail.com>
2020-04-25 09:51:35 +0400

    c++20's is_constant_evaluated()

--- Project/Project/src/cpp_weekly/constexpr.cpp
+++ Project/Project/src/cpp_weekly/constexpr.cpp
@@ -1,50 +1,24 @@
 #include <iostream>
+#include <type_traits>
 
-#include <array>
-
-struct Base
+// why is there such function?
+constexpr double tan1(double angle)
 {
-	// why on earth constexpr + virtual?
-	[[nodiscard]] constexpr virtual int get() const = 0;
-};
-
-struct Derived1 : Base
-{
-	[[nodiscard]] constexpr int get() const override
-	{
+	if /*constexpr*/ (std::is_constant_evaluated())
+		// do the fast thing that doesn't rely on inline assembly
 		return 5;
-	}
-};
-
-struct Derived2 : Base
-{
-	[[nodiscard]] constexpr int get() const override
-	{
+	else
+		// do the fast thing that does use inline assembly
 		return 10;
-	}
-};
-
-constexpr auto getSomeValues()
-{
-	const Derived1 d1_1;
-	const Derived1 d1_2;
-	const Derived1 d1_3;
-	const Derived2 d2_1;
-	const Derived2 d2_2;
-
-	const std::array<const Base *, 5> data
-	{
-		&d1_1, &d1_2, &d1_3, &d2_1, &d2_2
-	};
-
-	int sum = 0;
-	for (const auto *elem : data)
-		sum += elem->get();
-	return sum;
 }
 
 int main()
 {
-	constexpr auto v = getSomeValues();
-	return v;
+	std::cout << tan1(1) << '\n'; // 10
+
+	constexpr auto v = tan1(1);
+	std::cout << v << '\n'; // 5
+
+	const auto s = tan1(1);
+	std::cout << s << '\n'; // 5
 }
