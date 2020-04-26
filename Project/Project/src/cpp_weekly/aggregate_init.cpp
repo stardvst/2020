a91c5acbb470f97ccfae25e98439de0f051d6636
Lilia <lilia.manukyan@hotmail.com>
2020-04-26 08:57:32 +0400

    c++20 aggregates with user defined constructors

--- Project/Project/src/cpp_weekly/aggregate_init.cpp
+++ Project/Project/src/cpp_weekly/aggregate_init.cpp
@@ -2,40 +2,16 @@
 
 struct A
 {
+	A() = delete;
+	//A(int) {}
+	//A(int, int) {}
 	int m_i;
-};
-
-struct B
-{
-	B(const int i) : m_i(i)
-	{
-	}
-
-	int m_i;
-};
-
-struct S17Base
-{
-	double m_d;
-};
-
-struct S17 : S17Base
-{
-	int m_i;
-	float m_f;
+	int m_j;
 };
 
 int main()
 {
-	//A a(1); // cannot do; no ctor, so we add one, that's class B
-
-	B b(1);
-
-	// uniform init
-	S17Base base{ 33.5 };
-	std::cout << base.m_d << '\n';
-	//S17 sd{ 1, 2.3 }; // cannot init base
-
-	S17 sb{ { 54.8 }, 1, 2.3 };
-	std::cout << (sb.m_d + sb.m_f) << '\n';
+	// no instance of constructor "A::A" matches the argument list
+	// since ctor is deleted, c++20 rule
+	A a{ 1, 2 };
 }
