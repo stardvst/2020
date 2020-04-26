9c7a0ed6cbb6c00d5a83460df1e68a558e7df381
Lilia <lilia.manukyan@hotmail.com>
2020-04-26 09:17:35 +0400

    c++20's parameter packs in captures

--- Project/Project/src/cpp_weekly/lambdas.cpp
+++ Project/Project/src/cpp_weekly/lambdas.cpp
@@ -1,65 +1,40 @@
 #include <iostream>
 
-// #1: name 'fib' cannot be used before it is initialized
-//constexpr auto fib = [](int input)
-//{
-//	if (input < 2)
-//		return input;
-//	return fib(input - 1) + fib(input - 2);
-//};
-
-// #2
-constexpr auto fib = [](const auto fib, int input)
+// taking params by value, and copying into lambda
+template <typename Callable, typename ...Param>
+auto bindValues(Callable callable, Param ...params)
 {
-	if (input < 2)
-		return 1;
-	return fib(fib, input - 1) + fib(fib, input - 2);
-};
-
-// #3
-constexpr auto fibBase = [](const auto fib, int input)
-{
-	if (input < 2)
-		return 1;
-	return fibBase(fibBase, input - 1) + fibBase(fibBase, input - 2);
-};
-
-constexpr auto fibRec = [](int input)
-{
-	return fibBase(fibBase, input);
-};
-
-// #4
-constexpr auto fibRec2 = [](int input)
-{
-	auto fibLocal = [](const auto fibParam, int input)
+	/*return [=]()
 	{
-		if (input < 2)
-			return 1;
-		return fibParam(fibParam, input - 1) + fibParam(fibParam, input - 2);
+		return callable(params...);
+	};*/
+
+	//return [
+	//	callable = std::move(callable),
+
+	//	// syntax error : unexpected token '...', expected ','
+	//	params = std::move(params)...
+	//]()
+	//{
+	//	return callable(params...);
+	//};
+
+	return [
+		cb = std::move(callable),
+		...arg = std::move(params)
+	]()
+	{
+		return cb(arg...);
 	};
-	return fibLocal(fibLocal, input);
-};
+}
 
-// #5: mutually exclusive recursive lambdas
-auto fibMutExcl1 = [](const auto f1, const auto f2, int input)
+int add(int lhs, int rhs)
 {
-	if (input < 2)
-		return 1;
-	return input + f2(f1, f2, input - 1);
-};
-
-auto fibMutExcl2 = [](const auto f1, const auto f2, int input)
-{
-	if (input < 2)
-		return 1;
-	return input + f1(f1, f2, input - 1);
-};
+	return lhs + rhs;
+}
 
 int main()
 {
-	std::cout << fib(fib, 3) << '\n';
-	std::cout << fibRec(3) << '\n';
-	std::cout << fibRec2(3) << '\n';
-	std::cout << fibMutExcl1(fibMutExcl1, fibMutExcl2, 3) / 2 << '\n';
+	const auto bound = bindValues(add, 1, 3);
+	std::cout << bound() << '\n';
 }
