d98c9107d40a7422a8dc707d7fe90b8f5e986b42
Lilia <lilia.manukyan@hotmail.com>
2020-05-02 21:25:28 +0400

    `std::bind_front` implemented with lambdas

--- Project/Project/src/cpp_weekly/bind.cpp
+++ Project/Project/src/cpp_weekly/bind.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 #include <functional>
+#include <utility>
+
+template <typename Func, typename ...Params>
+auto bind_front(Func &&f, Params &&...params)
+{
+	// return how many params passed, bound to the callable
+	return[f = std::forward<Func>(f),
+		...boundParams = std::forward<Params>(params)]
+		(auto &&... restParams) -> decltype(auto) // perfect returning
+	{
+		return f(boundParams...,
+			std::forward<decltype(restParams)>(restParams)...);
+	};
+}
 
 int add(int i, int j, int k)
 {
@@ -10,7 +24,7 @@ int add(int i, int j, int k)
 int main()
 {
 	// binds first two arguments
-	auto func = std::bind_front(&add, 1, 2);
+	auto func = bind_front(&add, 1, 2);
 
 	// bind the 3rd argument
 	std::cout << func(3);
