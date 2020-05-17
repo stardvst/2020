f62e06d85d0241ff2ac6b09789c728fdf32599be
Lilia <lilia.manukyan@hotmail.com>
2020-05-17 11:28:32 +0400

    what is `requires requires`?

--- Project/Project/src/cpp_weekly/functions.cpp
+++ Project/Project/src/cpp_weekly/functions.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <functional>
 
-/*
-	callables:
-	1. lambda
-	2. free functions
-	3. member function pointers
-	4. member objects <---
-*/
-
-struct S
+template <typename Func>
+auto useFunc(Func obj)
 {
-	int i{10};
+	return obj();
+}
 
-	int get() const
-	{
-		return i;
-	}
-};
+// is a function, callable thing, but not a function object,
+// which is an abject having operator()
+int func()
+{
+	return 0;
+}
+
+// requires requires: define ad-hoc concept
+template <typename FuncObj>
+auto useFuncObj(FuncObj obj) requires requires { &FuncObj::operator(); }
+{
+	return obj();
+}
+
+// or, better
+template <typename T>
+concept hasCallOperator = requires { &T::operator(); };
+
+template <typename FuncObj>
+auto useFuncObjCon(FuncObj obj) requires hasCallOperator<FuncObj>
+{
+	return obj();
+}
 
 int main()
 {
-	S s;
-	//return s.i; // cannot do if i is not inited
-	std::cout << s.i << '\n';
+	useFunc([]() {});
+	useFunc(func);
 
-	int (S:: * mem_fn_ptr)() const = &S::get;
-	std::cout << (s.*mem_fn_ptr)() << '\n';
-
-	S *sPtr = &s;
-	std::cout << (sPtr->*mem_fn_ptr)() << '\n';
-
-	// member objects, not particular i, but i in general
-	int(S:: * mem_obj_ptr) = &S::i;
-	std::cout << s.*mem_obj_ptr << '\n';
-	std::cout << std::invoke(mem_obj_ptr, s) << '\n';
-
-	S s2;
-	s2.i = 5;
-	std::cout << s2.*mem_obj_ptr << '\n';
+	useFuncObj([]() {});
+	//useFuncObj(func);
+	useFuncObjCon([]() {});
+	//useFuncObjCon(func);
 }
