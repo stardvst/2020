53b08df152a583593d057f4eff10997011822d31
Lilia <lilia.manukyan@hotmail.com>
2020-05-18 11:10:11 +0400

    [[nodiscard]] ctors

--- Project/Project/src/cpp_weekly/constructors.cpp
+++ Project/Project/src/cpp_weekly/constructors.cpp
@@ -1,43 +1,26 @@
-struct S
+#include <memory>
+
+// if ctor acquires resources, make it nodiscard
+
+struct [[nodiscard]] S
 {
-	// any ctor can be explicit
-	/*explicit*/ S() = default;
-
-	explicit S(int, int)
-	{
-	}
-
-	S(int)
+	[[nodiscard]] S(int)
 	{
 	}
 };
 
-void f(const S &)
-{
-}
-
 S get()
 {
-	// cannot do if default ctor is explicit
-	//return {};
-	return S{};
-}
-
-S getOther()
-{
-	// cannot do if S(int, int) is explicit
-	//return { 2, 3 };
-	return S{ 2, 3 };
+	return S{ 5 };
 }
 
 int main()
 {
-	//f(10);
+	//S{ 5 };
 
-	f(S{ 10 });
-	f(static_cast<S>(10));
+	// warning C4834: discarding return value of function with 'nodiscard' attribute
+	get(); // no warning if type itself isn't 'nodiscard'
 
-	// default construct the parameter, but if default ctor is explicit, we'll get an error:
-	// 'void f(const S &)': cannot convert argument 1 from 'initializer list' to 'const S &'
-	f({});
+	// warning C4834: discarding return value of function with 'nodiscard' attribute
+	std::make_unique<int>( 5 );
 }
