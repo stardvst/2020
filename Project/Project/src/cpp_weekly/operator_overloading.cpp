441689019718ddbd5893b5a8017866c3ce3b605e
Lilia <lilia.manukyan@hotmail.com>
2020-06-26 22:35:56 +0400

    understanding operator overloading

--- Project/Project/src/cpp_weekly/operator_overloading.cpp
+++ Project/Project/src/cpp_weekly/operator_overloading.cpp
@@ -32,6 +32,64 @@ bool operator&&(const S &lhs, const S &rhs)
 	return lhs.value && rhs.value;
 }
 
+struct Complex
+{
+	Complex(const double R)
+		: r{ R }, i{ 0 }
+	{
+	}
+
+	constexpr Complex(const double R, const double I)
+		: r{ R }, i{ I }
+	{
+	}
+
+	// free + friend + inline function, specified in the body of the class,
+	// and all implicit conversions take place!
+	[[nodiscard]] friend constexpr Complex operator+(const Complex &lhs, const Complex &rhs)
+	{
+		return Complex{ lhs.r + rhs.r, lhs.i + rhs.i };
+	}
+
+	// problem with member function: cannot do this: 4.5 + c
+	/*
+	Complex operator+(const Complex &rhs)
+	{
+		return Complex{ r + rhs.r, i + rhs.i };
+	}
+	*/
+
+private:
+	double r;
+	double i;
+};
+
+// problem with free function: cannot access private members
+/*
+Complex operator+(const Complex &lhs, const Complex &rhs)
+{
+	return Complex{ lhs.r + rhs.r, lhs.i + rhs.i };
+}
+*/
+
+#include <iostream>
+#include <string>
+
+struct Pointer
+{
+	std::string *p;
+
+	constexpr auto *operator->()
+	{
+		return p;
+	}
+
+	constexpr auto *operator->() const
+	{
+		return p;
+	}
+};
+
 int main()
 {
 	// only first is called
@@ -48,4 +106,12 @@ int main()
 
 	// all are called - weird!
 	makeS(true) && (makeS(true) || makeS(false));
+
+	Complex c{ 1, 2 };
+	const auto r = 4.5 + c;
+	const auto result = c + Complex{ 3, 4 };
+
+	std::string s{ "Hello world" };
+	Pointer dumbPointer{ &s };
+	std::cout << dumbPointer->size() << '\n';
 }
